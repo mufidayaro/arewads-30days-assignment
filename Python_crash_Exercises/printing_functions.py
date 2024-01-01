430e28cf81f3585adcc865cc364212f68f9bb6ab
mufidayaro <garbamufida@yahoo.com>
2024-01-01 20:53:03 +0000

    Add Python Crash Exercises

---
+++ Python_crash_Exercises/printing_functions.py
@@ -0,0 +1,13 @@
+"""Put the functions for the example printing_models.py in a 
+separate file called printing_functions.py. Write an import statement at the top 
+of printing_models.py, and modify the file to use the imported functions.""" 
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    print("\nCompleted models:")
+    for model in completed_models:
+        print(model)        
